362f0fd2e61f485c2e2276bc3a5a4d141448f50a
Marlon Mavinier <mavinier2010@gmail.com>
2022-02-11 20:38:31 -0300

    tela para inserir os dados do boleto

--- lib/shared/themes/app_colors.dart
+++ lib/shared/themes/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static final primary = Color(0xFFFF941A);
   static final grey = Color(0xFF585666);
+  static final input = Color(0xFFB1B0B8);
   static final delete = Color(0xFFE83F5B);
   static final heading = Color(0xFF585666);
   static final body = Color(0xFF706E7A);
